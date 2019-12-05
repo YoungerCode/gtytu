5347554193a943786314349b290be6f07a7ead64
LordeUnicorn <milpontia.ngwenya@umuzi.org>
2019-12-05 10:08:15 +0200

    Updated Comibined code

--- combine_code.py
+++ combine_code.py
@@ -1,29 +1,16 @@
-def combine(list1, list2):
+  def combine(list1, list2):
+    lst = []
     len1 = len(list1)
     len2 = len(list2)
-    combo = []
-    if len1 > len2:
-        i = 0
-        while i < len2:
-            combo.append(list1[i])
-            combo.append(list2[i])
-            i = i + 1
-        for remaining_index in range(i,len1):
-            combo.append(list1[remaining_index])
-    elif len1 < len2:
-        i = 0
-        while i < len1:
-            combo.append(list1[i])
-            combo.append(list2[i])
-            i = i + 1
-        for remaining_index in range(i,len2):
-            combo.append(list2[remaining_index])
-    else:
-        if len1 == len2:
-            for i in range(len1):
-                combo.append(list1[i])
-                combo.append(list2[i])
-    return combo
+
+    for index in range( max(len1, len2) ):
+        if index+1 <= len1:
+            lst += [list1[index]]
+
+        if index+1 <= len2:
+            lst += [list2[index]]
+
+    return lst
 
 combined = combine([11,22,33], [1,2,3])
 print(combined)
